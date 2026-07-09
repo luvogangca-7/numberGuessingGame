8c21626cd4a4cc611a9aa6657f3bb7be4c79255b
Luvo Gangca <luvogangca@gmail.com>
2026-07-09 11:05:53 +0200

    created the user interface

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int maximum = 0;
+            Console.WriteLine("What Difficulty would you like? (E), (M), (H)");
+            string difficulty = Console.ReadLine();
+
+            if (difficulty == "E")
+            {
+                maximum = 10;
+            }
+            else if (difficulty == "M")
+            {
+                maximum = 50;
+            }
+            else
+            {
+                maximum = 1000;
+            }
+
+            NumberGuessing numberGuessing = new NumberGuessing(1, maximum);
+
+
+            bool guessed = false;
+
+            while (!guessed)
+            {
+                Console.WriteLine("Please enter your guess:");
+
+                string guess = Console.ReadLine();
+                GuessResult result = numberGuessing.MakeGuess(int.Parse(guess));
+
+
+
+                if (result == GuessResult.TooLow)
+                {
+                    Console.WriteLine("Your Guess is too LOW!  Please try again.");
+                }
+                else if (result == GuessResult.TooHigh)
+                {
+                    Console.WriteLine("Your Guess is too HIGH!  Please try again.");
+
+                }
+                else if (result == GuessResult.Correct)
+                {
+                    Console.WriteLine("Well Done!!!  You are correct.");
+                    guessed = true;
+
+                }
+                else if (result == GuessResult.OutOfLimits)
+                {
+                    Console.WriteLine("Out of range, are you kidding me?  Please try again.");
+                }
+
+            }
         }
     }
 }
